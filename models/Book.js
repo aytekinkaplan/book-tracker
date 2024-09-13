1cde7f12fdd0e40d0c204b4187d617f9a4105f22
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-09-13 23:44:10 +0300

    Up

--- models/Book.js
+++ models/Book.js
@@ -1,9 +1,15 @@
 const mongoose = require("mongoose");
 
-const BookSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  author: { type: String, required: true },
-  date: { type: Date, default: Date.now },
-});
+const BookSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true, trim: true },
+    author: { type: String, required: true },
+    date: { type: Date, default: Date.now },
+  },
+  {
+    collection: "books",
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("Book", BookSchema);
